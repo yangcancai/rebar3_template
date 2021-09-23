03c96175f8f6c69ed406d5d9a4d105d70915e5fd
yangcancai <yangcancai0112@gmail.com>
2021-09-23 15:54:15 +0800

    add: If not found cargo cmd , we will be auto install it

--- rust_nif_template/build_crates.sh
+++ rust_nif_template/build_crates.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+mkdir -p ./priv
+cmd_exists(){
+	local ret=0;
+	command -v $1 >/dev/null 2>&1 || { local ret=1; }
+	if [ "$ret" -ne 0 ]; then
+		return 0;
+	fi
+	return 1
+}
+cmd_exists cargo
+if [ $? -eq '0' ]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+else
+    echo "Rust already install."
+fi
 build(){
     mkdir -p {{app_dir}}/priv
     cargo build --manifest-path=crates/{{name}}/Cargo.toml --release
